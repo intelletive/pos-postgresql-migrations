03013a5340c07c55dabfd3e3d82a664d68afe6f1
Jacob Beasley <jacobbeasley@gmail.com>
2021-02-05 14:53:40 -0600

    Update run-migrations.sh

--- run-migrations.sh
+++ run-migrations.sh
@@ -1,4 +1,4 @@
-# Run migration scripts in alphabetical order against a remote postgres database
+# Run migration scripts in alphabetical order against a remote mysql database
 
 # Loop through migrations (quitting if one fails)
 set -e
@@ -7,12 +7,15 @@ do
 	# Build Script File, auto-embedding script filename and db schema name
 	echo "\n\n---------------------------------------------------------"
 	echo RUNNING MIGRATION $FILE
-	echo "SET search_path TO $PSQL_SCHEMA;" > temp.sql
-	cat $FILE >> temp.sql
-	sed -i "s/INSERT_SCRIPT_NAME_HERE/$FILE/g" temp.sql || true
+	rm -f temp.sql
+	rm -f tempbuffer.sql
+	echo "SET search_path TO $PSQL_SCHEMA;" > tempbuffer.sql
+	cat $FILE >> tempbuffer.sql
+	sed "s+INSERT_SCRIPT_NAME_HERE+$FILE+g" tempbuffer.sql > temp.sql
 
 	# Run migration
 	psql $PSQL_EXTRA_CLI_PARAMS --file=temp.sql
-    rm -f temp.sql
+	rm -f temp.sql
+	rm -f tempbuffer.sql
 	echo "DONE RUNNING MIGRATION $FILE\n"
 done
